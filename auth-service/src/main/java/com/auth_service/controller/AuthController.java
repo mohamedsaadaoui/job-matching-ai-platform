6177ae6789cec1e93f397767b8a84890f73510da
Mohamed Saadaoui <mohamed.saadaoui.1@esprit.tn>
2026-05-01 19:15:03 +0100

    Initial commit

---
+++ auth-service/src/main/java/com/auth_service/controller/AuthController.java
@@ -0,0 +1,27 @@
+package com.auth_service.controller;
+
+import com.auth_service.entity.User;
+import com.auth_service.repo.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final UserRepository userRepository;
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @PostMapping("/login")
+    public String login() {
+        return "JWT_TOKEN_HERE"; // on fera JWT après
+    }
+}
---
+++ auth-service/src/main/java/com/auth_service/repo/UserRepository.java
@@ -0,0 +1,10 @@
+package com.auth_service.repo;
+
+import com.auth_service.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
